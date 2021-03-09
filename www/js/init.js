ffe9797953e5d18693f04e04f766d36eca3849be
bycarlos28 <carlosfernandez28correa@gmail.com>
2021-03-09 17:47:56 +0100

    primer commit

---
+++ www/js/init.js
@@ -0,0 +1,31 @@
+(function($){
+  $(function(){
+
+      // on document ready...
+
+      // init GUI
+    $('.sidenav').sidenav();
+      $('.fixed-action-btn').floatingActionButton();
+      // els tabs necessiten ser swipeable
+      //$('.tabs').tabs({"swipeable":true});
+
+    var options = { "swipeable": true };
+    var el = document.getElementById('tabs-swipe-demo');
+    var tabsInstance = M.Tabs.init(el, options);
+
+    // make clicable an element of the list and go to the next tab
+
+    $('.collection-item.active').click(function() {
+        tabsInstance.select("test-swipe-2");
+    });
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady() {
+    // Cordova is now initialized. Have fun!
+
+    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version)
+}
